cdf60d6d9a5d3b165a8e1ae1d62e6115df434975
Martin Šošić <Martinsos@users.noreply.github.com>
2023-11-14 14:03:10 +0100

    Improved charts, adjusted age groups, added unique users table, added numPeriods option to discord. (#14)
    * Improved charts, adjusted age groups, added unique users table, added
    numPeriods option to discord.
    
    * fix

--- src/analytics/reports/index.js
+++ src/analytics/reports/index.js
@@ -6,16 +6,16 @@ const { generatePeriodReport } = require('./periodReport')
 // TODO: Optimize, I mostly ignored performance for now since we haven't had the amount of
 // events where it would matter, but now we are getting there.
 
-async function generateDailyReport(prefetchedEvents = undefined) {
-  return generatePeriodReport(14, 'day', prefetchedEvents, false)
+async function generateDailyReport (prefetchedEvents = undefined, numPeriods = undefined) {
+  return generatePeriodReport(numPeriods ?? 14, 'day', prefetchedEvents, false)
 }
 
-async function generateWeeklyReport(prefetchedEvents = undefined) {
-  return generatePeriodReport(12, 'week', prefetchedEvents)
+async function generateWeeklyReport (prefetchedEvents = undefined, numPeriods = undefined) {
+  return generatePeriodReport(numPeriods ?? 12, 'week', prefetchedEvents)
 }
 
-async function generateMonthlyReport(prefetchedEvents = undefined) {
-  return generatePeriodReport(12, 'month', prefetchedEvents)
+async function generateMonthlyReport (prefetchedEvents = undefined, numPeriods = undefined) {
+  return generatePeriodReport(numPeriods ?? 12, 'month', prefetchedEvents)
 }
 
 module.exports = {
--- src/analytics/reports/periodReport/common.js
+++ src/analytics/reports/periodReport/common.js
@@ -1,47 +1,5 @@
 const moment = require('../../moment')
 
-const { executionEnvs } = require('../../executionEnvs')
-
-const { calcUserAgeInDays, elemFromBehind } = require('../utils')
-
-
-function calcUniqueNonLocalEventsInPeriod(periods, eventsByEnv) {
-  const uniqueNonLocalEventsInPeriod = {};
-  for (let envKey of Object.keys(executionEnvs)) {
-    const events = eventsByEnv[envKey] ?? []
-    uniqueNonLocalEventsInPeriod[envKey] = uniqueUserIdsInPeriod(events, elemFromBehind(periods, 0)).length
-  }
-  return uniqueNonLocalEventsInPeriod;
-}
-
-// The main metric we are calculating -> for each period, number of unique users, grouped by age (of usage).
-// We return it ready for displaying via chart or table.
-// Takes list of all events, ends of all periods, and period duration.
-function calcUniqueLocalEventsPerPeriodByAge (userEvents, periods) {
-  const uniqueLocalEventsPerPeriodByAge = {
-    // All series have the same length, which is the length of .periodEnds.
-    series: {
-      ">30d": [], // [number]
-      "(5, 30]d": [],
-      "(1, 5]d": [],
-      "<=1d": []
-    },
-    periodEnds: [] // [string] where strings are dates formatted as YY-MM-DD.
-  }
-  for (let period of periods) {
-    const userEventsUpToAndInPeriod = filterEventsUpToAndInPeriod(userEvents, period)
-    const ids = uniqueUserIdsInPeriod(userEventsUpToAndInPeriod, period)
-    const ages = ids.map(id => calcUserAgeInDays(userEventsUpToAndInPeriod, id))
-    uniqueLocalEventsPerPeriodByAge.series["<=1d"].push(ages.filter(age => age <= 1).length)
-    uniqueLocalEventsPerPeriodByAge.series["(1, 5]d"].push(ages.filter(age => age > 1 && age <= 5).length)
-    uniqueLocalEventsPerPeriodByAge.series["(5, 30]d"].push(ages.filter(age => age > 5 && age <= 30).length)
-    uniqueLocalEventsPerPeriodByAge.series[">30d"].push(ages.filter(age => age > 30).length)
-
-    uniqueLocalEventsPerPeriodByAge.periodEnds.push(period[1].format('YY-MM-DD'))
-  }
-  return uniqueLocalEventsPerPeriodByAge
-}
-
 // Takes a bunch of events that have .timestamp field and returns only those that
 // happened in the period specified via (startTime, endTime).
 function filterEventsInPeriod (events, [startTime, endTime]) {
@@ -50,7 +8,7 @@ function filterEventsInPeriod (events, [startTime, endTime]) {
 
 // Takes a bunch of events that have .timestamp field and returns only those that
 // happened in the period specified via (startTime, endTime) or before it.
-function filterEventsUpToAndInPeriod (events, [startTime, endTime]) {
+function filterEventsUpToAndInPeriod (events, [_startTime, endTime]) {
   return events.filter(e => moment(e.timestamp).isSameOrBefore(endTime))
 }
 
@@ -74,8 +32,6 @@ function uniqueUserIdsInPeriod (events, period) {
 }
 
 module.exports = {
-  calcUniqueNonLocalEventsInPeriod,
-  calcUniqueLocalEventsPerPeriodByAge,
   filterEventsInPeriod,
   filterEventsUpToAndInPeriod,
   calcLastNPeriods,
