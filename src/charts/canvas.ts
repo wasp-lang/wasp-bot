49ef66ec1ac2877b26c72a2ca9403600f001dab4
Franjo Mindek <franjo.mindek@gmail.com>
2025-04-18 17:50:09 +0200

    comments

--- src/charts/canvas.ts
+++ src/charts/canvas.ts
@@ -2,12 +2,12 @@ import { Chart, ChartConfiguration } from "chart.js";
 import { MatrixController, MatrixElement } from "chartjs-chart-matrix";
 import { ChartJSNodeCanvas, MimeType } from "chartjs-node-canvas";
 
-// init
+// Chart.js initialization.
 Chart.register(MatrixController, MatrixElement);
 
 // NOTE:
-// we want to reuse canvases because of memory management
-// if we want different sizes we need to create multiple canvases
+// We reuse the same canvas instance for memory efficiency.
+// If different chart sizes are needed, create multiple canvas instances.
 
 const canvasConfiguration = {
   width: 800,
