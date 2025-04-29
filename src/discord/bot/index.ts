065cb86daea4f1ffd29c8e2a763769ef9ff6ea24
Franjo Mindek <franjo.mindek@gmail.com>
2025-04-29 12:41:02 +0200

    refacotr

--- src/discord/bot/index.ts
+++ src/discord/bot/index.ts
@@ -1,6 +1,5 @@
 import Discord from "discord.js";
 import { config as dotenvConfig } from "dotenv";
-import _ from "lodash";
 import schedule from "node-schedule";
 
 import logger from "../../utils/logger";
@@ -23,44 +22,53 @@ const TIME_ZONE = "Europe/Zagreb";
 export function start(): void {
   const discordClient = new Discord.Client({});
 
-  discordClient.on("ready", _.partial(handleReady, discordClient));
-  discordClient.on("message", _.partial(handleMessage, discordClient));
-  discordClient.on(
-    "messageUpdate",
-    _.partial(handleMessageUpdate, discordClient),
-  );
+  discordClient.on("ready", () => {
+    logger.info(`Logged in as: ${discordClient.user?.tag}.`);
+
+    scheduleDailyStandup(discordClient);
+    scheduleDailyAnalyticsReport(discordClient);
+  });
+
+  discordClient.on("message", handleMessage);
+  async function handleMessage(message: Discord.Message): Promise<void> {
+    if (isOurDiscordBotMessage(discordClient, message)) {
+      return;
+    }
+
+    if (isIntroductionMessage(message)) {
+      await handleIntroductionMessage(message);
+    } else if (isAnalyticsCommand(message)) {
+      await handleAnalyticsCommand(discordClient, message);
+    }
+  }
+
+  discordClient.on("messageUpdate", async (_oldMessage, newMessage) => {
+    // TODO: Actually handle partial messages.
+    // For that we first need to enable them in Discord.Client.
+    // https://github.com/zziger/discord.js-selfbot/blob/master/docs/topics/partials.md
+    if (!(newMessage instanceof Discord.Message)) {
+      return;
+    }
+
+    await handleMessage(newMessage);
+  });
 
   discordClient.login(BOT_TOKEN);
 }
 
-async function handleReady(discordClient: Discord.Client): Promise<void> {
-  logger.info(`Logged in as: ${discordClient.user?.tag}.`);
-
-  // Initiates daily standup every work day at 8:00 am.
+function scheduleDailyStandup(discordClient: Discord.Client): void {
+  // Initiate daily standup every work day at 8:00 am.
   schedule.scheduleJob(
     { dayOfWeek: [1, 2, 3, 4, 5], hour: 8, minute: 0, tz: TIME_ZONE },
     () => initiateDailyStandup(discordClient),
   );
+}
 
-  // Sends daily analytics reports every day at 7:00 am.
+function scheduleDailyAnalyticsReport(discordClient: Discord.Client): void {
+  // Send analytics reports every day at 7:00 am.
   schedule.scheduleJob({ hour: 7, minute: 0, tz: TIME_ZONE }, () =>
     sendDailyAnalyticsReport(discordClient),
   );
-}
-
-async function handleMessage(
-  discordClient: Discord.Client,
-  message: Discord.Message,
-): Promise<void> {
-  if (isOurDiscordBotMessage(discordClient, message)) {
-    return;
-  }
-
-  if (isIntroductionMessage(message)) {
-    await handleIntroductionMessage(message);
-  } else if (isAnalyticsCommand(message)) {
-    await handleAnalyticsCommand(discordClient, message);
-  }
 }
 
 function isOurDiscordBotMessage(
@@ -68,19 +76,4 @@ function isOurDiscordBotMessage(
   message: Discord.Message,
 ) {
   return message.author.id === discordClient.user?.id;
-}
-
-// TODO: Actually handle partial messages.
-// For that we first need to enable them in Discord.Client.
-// https://github.com/zziger/discord.js-selfbot/blob/master/docs/topics/partials.md
-async function handleMessageUpdate(
-  discordClient: Discord.Client,
-  _oldMessage: Discord.Message | Discord.PartialMessage,
-  newMessage: Discord.Message | Discord.PartialMessage,
-): Promise<void> {
-  if (!(newMessage instanceof Discord.Message)) {
-    return;
-  }
-
-  await handleMessage(discordClient, newMessage);
 }
