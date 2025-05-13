1b62889f2e41b6f40379730a792cf7a98c8bc222
Franjo Mindek <84568328+FranjoMindek@users.noreply.github.com>
2025-05-13 12:15:03 +0200

    Refactor discord bot logic into multiple files (#45)
    * split single file
    
    * awaits and types
    
    * unnecessary memeber
    
    * comments -> functions
    
    * name, structyure
    
    * functions
    
    * de-modularaize a bit
    
    * comment
    
    * login last
    
    * analytics
    
    * quote
    
    * type external quote
    
    * imports
    
    * handlers, remove role id from channel ids
    
    * refactors
    
    * server ids
    
    * refacotr
    
    * refactor 2
    
    * update help
    
    * revert
    
    * Update src/discord/bot/analytics/daily-report.ts
    
    Co-authored-by: Martin Šošić <Martinsos@users.noreply.github.com>
    
    * naming
    
    * commands
    
    * fix regex dep
    
    * extract prefix instead
    
    * improve naming
    
    ---------
    
    Co-authored-by: Martin Šošić <Martinsos@users.noreply.github.com>

---
+++ src/discord/bot/introduction.ts
@@ -0,0 +1,41 @@
+import Discord from "discord.js";
+
+import logger from "../../utils/logger";
+import { GUEST_ROLE_ID, INTRODUCTIONS_CHANNEL_ID } from "../server-ids";
+
+export function isIntroductionMessage(message: Discord.Message) {
+  return isIntroductionsChannel(message.channel) && isGuestUser(message);
+}
+
+function isIntroductionsChannel(channel: Discord.Channel): boolean {
+  return channel.id.toString() === INTRODUCTIONS_CHANNEL_ID;
+}
+
+function isGuestUser(message: Discord.Message): boolean {
+  const member = message.guild?.member(message.author);
+  return !!member?.roles.cache.get(GUEST_ROLE_ID);
+}
+
+export async function handleIntroductionMessage(
+  message: Discord.Message,
+): Promise<void> {
+  const trimmedMessageLength = message.content.trim().length;
+  if (trimmedMessageLength < 20) {
+    await message.reply(
+      "👋 Great to have you here! Please introduce yourself with a message that's at least 2️⃣0️⃣ characters long and I will give you full access to the server.",
+    );
+    return;
+  }
+
+  try {
+    const member = message.guild?.member(message.author);
+    await member?.roles.remove(GUEST_ROLE_ID);
+
+    await message.reply(
+      "Nice getting to know you ☕️! You now have full access to the Wasp Discord Server 🐝. Welcome!",
+    );
+  } catch (error) {
+    logger.error(error);
+    await message.reply(`Error: ${error}`);
+  }
+}
